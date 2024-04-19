7351222744ea5411a29b83affd297ebd45fe47aa
JBakes98 <34064544+JBakes98@users.noreply.github.com>
2024-04-19 23:44:05 +0100

    Added Serilog logging

---
+++ src/OrderFlow.Api/Extensions/LoggingExtensions.cs
@@ -0,0 +1,24 @@
+using Serilog;
+using Serilog.Exceptions;
+
+namespace OrderFlow.Extensions;
+
+public static class LoggingExtensions
+{
+    public static void RegisterLogging(this WebApplicationBuilder builder, IConfiguration configuration)
+    {
+        Log.Logger = new LoggerConfiguration()
+            .ReadFrom.Configuration(configuration)
+            .Enrich.FromLogContext()
+            .Enrich.WithExceptionDetails()
+            .Enrich.WithProperty("Env", builder.Environment.EnvironmentName)
+            .CreateLogger();
+
+        builder.Host.UseSerilog();
+        
+        Log.Logger.Information("Application starting...");
+        
+        if (builder.Environment.IsDevelopment())
+            Log.Logger.Debug(configuration.Dump());
+    }
+}
--- src/OrderFlow.Api/Program.cs
+++ src/OrderFlow.Api/Program.cs
@@ -3,6 +3,7 @@ using OrderFlow.Extensions;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.RegisterServices(builder.Configuration);
+builder.RegisterLogging(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
