e5f59c6ee092a11d49776d0f9a0cd19647972f05
JBakes98 <34064544+JBakes98@users.noreply.github.com>
2024-04-19 17:00:09 +0100

    Add error object

---
+++ src/OrderFlow.Api/Models/Error.cs
@@ -0,0 +1,15 @@
+using System.Net;
+
+namespace OrderFlow.Models;
+
+public class Error
+{
+    public Error(HttpStatusCode errorType, IEnumerable<string> errorCodes)
+    {
+        ErrorType = errorType;
+        ErrorCodes = errorCodes;
+    }
+
+    public IEnumerable<string> ErrorCodes { get; }
+    public HttpStatusCode ErrorType { get; }
+}
