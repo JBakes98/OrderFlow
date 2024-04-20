ad02789574123a163148fc5ad1ed258681cf751f
JBakes98 <34064544+JBakes98@users.noreply.github.com>
2024-04-20 19:50:45 +0100

    Remove uneccessary declaration

--- src/OrderFlow.Api/Services/Handlers/IOrderHandler.cs
+++ src/OrderFlow.Api/Services/Handlers/IOrderHandler.cs
@@ -5,5 +5,5 @@ namespace OrderFlow.Services.Handlers;
 
 public interface IOrderHandler<in TRequest>
 {
-    public Task<OneOf<Order, Error>> HandleAsync(TRequest request, CancellationToken cancellationToken);
+    Task<OneOf<Order, Error>> HandleAsync(TRequest request, CancellationToken cancellationToken);
 }
