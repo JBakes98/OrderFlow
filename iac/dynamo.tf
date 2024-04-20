11cc706c910e54b02db9a8fb29a1f6f9adc02151
JBakes98 <34064544+JBakes98@users.noreply.github.com>
2024-04-20 20:13:26 +0100

    Add instrument dynamo table

--- iac/dynamo.tf
+++ iac/dynamo.tf
@@ -2,10 +2,10 @@ resource "aws_dynamodb_table" "order_table" {
   name           = "Order"
   billing_mode   = "PROVISIONED"
   hash_key       = "Id"
-  read_capacity  = 5  # Adjust according to your read/write throughput requirements
+  read_capacity  = 5 # Adjust according to your read/write throughput requirements
   write_capacity = 5
-  
-  stream_enabled = true
+
+  stream_enabled   = true
   stream_view_type = "NEW_IMAGE"
 
   attribute {
@@ -17,7 +17,7 @@ resource "aws_dynamodb_table" "order_table" {
     name = "InstrumentId"
     type = "S"
   }
-  
+
   attribute {
     name = "Price"
     type = "N"
@@ -25,16 +25,29 @@ resource "aws_dynamodb_table" "order_table" {
 
   # Secondary index definition
   global_secondary_index {
-    name               = "InstrumentPriceGSI"
-    hash_key           = "InstrumentId"
-    projection_type    = "ALL"
-    read_capacity      = 5
-    write_capacity     = 5
+    name            = "InstrumentPriceGSI"
+    hash_key        = "InstrumentId"
+    projection_type = "ALL"
+    read_capacity   = 5
+    write_capacity  = 5
 
     # Sort key definition for the GSI
     range_key = "Price"
 
     # Define the attributes projected into the index
-    non_key_attributes = []  # Add attribute names here if projection_type is "INCLUDE"
+    non_key_attributes = [] # Add attribute names here if projection_type is "INCLUDE"
+  }
+}
+
+resource "aws_dynamodb_table" "instrument_table" {
+  name           = "Instrument"
+  billing_mode   = "PROVISIONED"
+  hash_key       = "Id"
+  read_capacity  = 5 # Adjust according to your read/write throughput requirements
+  write_capacity = 5
+
+  attribute {
+    name = "Id"
+    type = "S"
   }
 }
