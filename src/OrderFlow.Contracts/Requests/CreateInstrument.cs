5f891a631fbbf9cb6f0dbab5800a90f545e51d63
JBakes98 <34064544+JBakes98@users.noreply.github.com>
2024-04-22 12:53:02 +0100

    Add ticker to create request and mapper

--- src/OrderFlow.Contracts/Requests/CreateInstrument.cs
+++ src/OrderFlow.Contracts/Requests/CreateInstrument.cs
@@ -2,12 +2,14 @@ namespace OrderFlow.Contracts.Requests;
 
 public class CreateInstrument
 {
+    public string Ticker { get; set; }
     public string Name { get; set; }
     public string Sector { get; set; }
     public string Exchange { get; set; }
 
-    public CreateInstrument(string name, string sector, string exchange)
+    public CreateInstrument(string ticker, string name, string sector, string exchange)
     {
+        Ticker = ticker;
         Name = name;
         Sector = sector;
         Exchange = exchange;
