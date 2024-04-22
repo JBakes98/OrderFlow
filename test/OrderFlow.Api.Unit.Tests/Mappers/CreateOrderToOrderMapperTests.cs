bc735f782f550a850c51dd480ef5202a59a69dca
JBakes98 <34064544+JBakes98@users.noreply.github.com>
2024-04-22 12:53:35 +0100

    Added CreateOrderToOrderMapperTests.cs

---
+++ test/OrderFlow.Api.Unit.Tests/Mappers/CreateOrderToOrderMapperTests.cs
@@ -0,0 +1,24 @@
+using OrderFlow.Contracts.Requests;
+using OrderFlow.Mappers.Request;
+
+namespace OrderFlow.Api.Unit.Tests.Mappers;
+
+public class CreateOrderToOrderMapperTests
+{
+    [Theory, AutoMoqData]
+    public void Should_Map_CreateOrder_To_Order(
+        CreateOrder source,
+        CreateOrderToOrderMapper sut
+    )
+    {
+        var result = sut.Map(source);
+        
+        Assert.Equal(source.InstrumentId.ToString(), result.InstrumentId);
+        Assert.Equal(source.Quantity, result.Quantity);
+        Assert.Equal(source.Price, result.Price);
+        
+        Assert.NotNull(result.Id);
+        Assert.NotNull(result.OrderDate);
+    }
+    
+}
