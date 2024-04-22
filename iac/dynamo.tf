17e834ef70e198ba2a8d45d1015f75c22826d20c
JBakes98 <34064544+JBakes98@users.noreply.github.com>
2024-04-22 17:35:08 +0100

    Create new OrderEvents dynamodb table, simplified existing tables

--- iac/dynamo.tf
+++ iac/dynamo.tf
@@ -5,37 +5,25 @@ resource "aws_dynamodb_table" "order_table" {
   read_capacity  = 5 # Adjust according to your read/write throughput requirements
   write_capacity = 5
 
-  stream_enabled   = true
-  stream_view_type = "NEW_IMAGE"
-
   attribute {
     name = "Id"
     type = "S"
   }
+}
+
+resource "aws_dynamodb_table" "order_events_table" {
+  name           = "OrderEvents"
+  billing_mode   = "PROVISIONED"
+  hash_key       = "StreamId"
+  read_capacity  = 5 # Adjust according to your read/write throughput requirements
+  write_capacity = 5
+
+  stream_enabled   = true
+  stream_view_type = "NEW_IMAGE"
 
   attribute {
-    name = "InstrumentId"
+    name = "StreamId"
     type = "S"
-  }
-
-  attribute {
-    name = "Price"
-    type = "N"
-  }
-
-  # Secondary index definition
-  global_secondary_index {
-    name            = "InstrumentPriceGSI"
-    hash_key        = "InstrumentId"
-    projection_type = "ALL"
-    read_capacity   = 5
-    write_capacity  = 5
-
-    # Sort key definition for the GSI
-    range_key = "Price"
-
-    # Define the attributes projected into the index
-    non_key_attributes = [] # Add attribute names here if projection_type is "INCLUDE"
   }
 }
 
