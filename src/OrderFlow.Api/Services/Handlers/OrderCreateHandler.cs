15fbe851c3fe111b94b4f7f7fe16e17affc24447
JBakes98 <34064544+JBakes98@users.noreply.github.com>
2024-04-22 17:34:35 +0100

    Use the new event services in the OrderCreateHandler.cs to raise events with each new order

--- src/OrderFlow.Api/Services/Handlers/OrderCreateHandler.cs
+++ src/OrderFlow.Api/Services/Handlers/OrderCreateHandler.cs
@@ -1,5 +1,6 @@
 using OneOf;
 using OrderFlow.Contracts.Requests;
+using OrderFlow.Events;
 using OrderFlow.Extensions;
 using OrderFlow.Models;
 using OrderFlow.Repositories;
@@ -9,16 +10,22 @@ namespace OrderFlow.Services.Handlers;
 public class OrderCreateHandler : IHandler<CreateOrder, Order>
 {
     private readonly IMapper<CreateOrder, Order> _createOrderToOrderMapper;
+    private readonly IMapper<Order, OrderCreatedEvent> _orderToOrderCreatedEventMapper;
     private readonly IRepository<Order> _repository;
+    private readonly IRepository<DomainEvent> _domainRepository;
     private readonly IInstrumentService _instrumentService;
 
     public OrderCreateHandler(
         IMapper<CreateOrder, Order> createOrderToOrderMapper,
+        IMapper<Order, OrderCreatedEvent> orderToOrderCreatedEventMapper,
         IRepository<Order> repository,
+        IRepository<DomainEvent> domainRepository,
         IInstrumentService instrumentService)
     {
         _createOrderToOrderMapper = createOrderToOrderMapper;
+        _orderToOrderCreatedEventMapper = orderToOrderCreatedEventMapper;
         _repository = repository;
+        _domainRepository = domainRepository;
         _instrumentService = instrumentService;
     }
     public async Task<OneOf<Order, Error>> HandleAsync(CreateOrder request, CancellationToken cancellationToken)
@@ -31,6 +38,9 @@ public class OrderCreateHandler : IHandler<CreateOrder, Order>
         var order = _createOrderToOrderMapper.Map(request);
         
         await _repository.InsertAsync(order, cancellationToken);
+        
+        var @event = _orderToOrderCreatedEventMapper.Map(order);
+        await _domainRepository.InsertAsync(@event, cancellationToken);
         
         return order;
     }
