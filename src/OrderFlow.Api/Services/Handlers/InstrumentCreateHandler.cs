7b7b01f285f62cfa6b72937aaac75d65b5671fbb
JBakes98 <34064544+JBakes98@users.noreply.github.com>
2024-04-22 12:52:06 +0100

    Add instrument handlers

---
+++ src/OrderFlow.Api/Services/Handlers/InstrumentCreateHandler.cs
@@ -0,0 +1,31 @@
+using OneOf;
+using OrderFlow.Contracts.Requests;
+using OrderFlow.Extensions;
+using OrderFlow.Models;
+using OrderFlow.Repositories;
+using Instrument = OrderFlow.Models.Instrument;
+
+namespace OrderFlow.Services.Handlers;
+
+public class InstrumentCreateHandler : IHandler<CreateInstrument, Instrument>
+{
+    private readonly IRepository<Instrument> _instrumentRepository;
+    private readonly IMapper<CreateInstrument, Instrument> _createInstrumentToInstrumentMapper;
+
+    public InstrumentCreateHandler(
+        IMapper<CreateInstrument, Instrument> createInstrumentToInstrumentMapper,
+        IRepository<Instrument> instrumentRepository)
+    {
+        _instrumentRepository = instrumentRepository;
+        _createInstrumentToInstrumentMapper = createInstrumentToInstrumentMapper;
+    }
+    
+    public async Task<OneOf<Instrument, Error>> HandleAsync(CreateInstrument request, CancellationToken cancellationToken)
+    {
+        var instrument = _createInstrumentToInstrumentMapper.Map(request);
+            
+        await _instrumentRepository.InsertAsync(instrument, cancellationToken);
+
+        return instrument;
+    }
+}
---
+++ src/OrderFlow.Api/Services/Handlers/InstrumentGetHandler.cs
@@ -0,0 +1,23 @@
+using Amazon.DynamoDBv2.DataModel;
+using OneOf;
+using OrderFlow.Models;
+using OrderFlow.Repositories;
+
+namespace OrderFlow.Services.Handlers;
+
+public class InstrumentGetHandler : IHandler<Guid, Instrument>
+{
+    private readonly IRepository<Instrument> _repository;
+
+    public InstrumentGetHandler(IRepository<Instrument> repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task<OneOf<Instrument, Error>> HandleAsync(Guid request, CancellationToken cancellationToken)
+    {
+        var instrument= await _repository.GetByIdAsync(request.ToString());
+
+        return instrument;
+    }
+}
