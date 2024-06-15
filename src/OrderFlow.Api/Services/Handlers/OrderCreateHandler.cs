a4c56376a88f1464159297e4b9e8f89f4b6ea939
JBakes98 <34064544+JBakes98@users.noreply.github.com>
2024-06-15 09:53:27 +0100

    Update order create handler to raise event when order created

--- src/OrderFlow.Api/Services/Handlers/OrderCreateHandler.cs
+++ src/OrderFlow.Api/Services/Handlers/OrderCreateHandler.cs
@@ -11,22 +11,25 @@ public class OrderCreateHandler : IHandler<CreateOrder, Order>
 {
     private readonly IMapper<CreateOrder, Order> _createOrderToOrderMapper;
     private readonly IMapper<Order, OrderCreatedEvent> _orderToOrderCreatedEventMapper;
+    private readonly IMapper<BaseOrderEvent, Event> _orderEventToEventMapper;
     private readonly IRepository<Order> _repository;
-    private readonly IRepository<DomainEvent> _domainRepository;
+    private readonly IRepository<Event> _eventRepository;
     private readonly IInstrumentService _instrumentService;
 
     public OrderCreateHandler(
         IMapper<CreateOrder, Order> createOrderToOrderMapper,
         IMapper<Order, OrderCreatedEvent> orderToOrderCreatedEventMapper,
         IRepository<Order> repository,
-        IRepository<DomainEvent> domainRepository,
-        IInstrumentService instrumentService)
+        IRepository<Event> eventRepository,
+        IInstrumentService instrumentService, 
+        IMapper<BaseOrderEvent, Event> orderEventToEventMapper)
     {
         _createOrderToOrderMapper = createOrderToOrderMapper;
         _orderToOrderCreatedEventMapper = orderToOrderCreatedEventMapper;
         _repository = repository;
-        _domainRepository = domainRepository;
+        _eventRepository = eventRepository;
         _instrumentService = instrumentService;
+        _orderEventToEventMapper = orderEventToEventMapper;
     }
     public async Task<OneOf<Order, Error>> HandleAsync(CreateOrder request, CancellationToken cancellationToken)
     {
@@ -39,8 +42,9 @@ public class OrderCreateHandler : IHandler<CreateOrder, Order>
 
         await _repository.InsertAsync(order, cancellationToken);
 
-        var @event = _orderToOrderCreatedEventMapper.Map(order);
-        await _domainRepository.InsertAsync(@event, cancellationToken);
+        var orderEvent = _orderToOrderCreatedEventMapper.Map(order);
+        var @event = _orderEventToEventMapper.Map(orderEvent);
+        await _eventRepository.InsertAsync(@event, cancellationToken);
 
         return order;
     }
