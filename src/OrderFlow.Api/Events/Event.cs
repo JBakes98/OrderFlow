b48edf8735b3399ff879af2381d0d7f34145de9c
JBakes98 <34064544+JBakes98@users.noreply.github.com>
2024-06-15 09:55:46 +0100

    Added base event object that is primitive stored type

---
+++ src/OrderFlow.Api/Events/Event.cs
@@ -0,0 +1,15 @@
+using System.Text.Json.Serialization;
+using Amazon.DynamoDBv2.DataModel;
+
+namespace OrderFlow.Events;
+
+[DynamoDBTable("Events")]
+public class Event
+{
+    public string Id { get; } = Guid.NewGuid().ToString();
+    [JsonPropertyName("Event")] public string @event { get; set; }
+    public DateTime Timestamp { get; } = DateTime.Now;
+    public string StreamId { get; set; }
+    public string EventType { get; set; }
+    public long _deletionDate { get; } = ((DateTimeOffset)DateTime.Now.AddDays(7)).ToUnixTimeSeconds();
+}
