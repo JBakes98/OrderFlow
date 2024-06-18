be018d6d17e69fde4424fd800d3bd3362e27f547
JBakes98 <34064544+JBakes98@users.noreply.github.com>
2024-06-18 08:38:05 +0100

    Simplified Order and Instrument objects as it doesnt need Dynanmo annotations for Postgres

--- src/OrderFlow.Api/Models/Instrument.cs
+++ src/OrderFlow.Api/Models/Instrument.cs
@@ -1,14 +1,7 @@
-using Amazon.DynamoDBv2.DataModel;
-
 namespace OrderFlow.Models;
 
-[DynamoDBTable("Instrument")]
 public class Instrument
 {
-    public Instrument()
-    {
-    }
-    
     public Instrument(string ticker, string name, string sector, string exchange)
     {
         Ticker = ticker;
@@ -16,8 +9,6 @@ public class Instrument
         Sector = sector;
         Exchange = exchange;
     }
-
-    [DynamoDBHashKey]
     public string Id { get; set; } = Guid.NewGuid().ToString();
     public string Ticker { get; set; }
     public string Name { get; set; }
