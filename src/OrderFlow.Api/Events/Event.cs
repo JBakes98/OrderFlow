38275dcfccf3b0bd6d792b0624beeff34b33a184
JBakes98 <34064544+JBakes98@users.noreply.github.com>
2024-06-18 08:38:26 +0100

    Added Key annotation to Event object as it was not migrating correctly without it

--- src/OrderFlow.Api/Events/Event.cs
+++ src/OrderFlow.Api/Events/Event.cs
@@ -1,15 +1,13 @@
-using System.Text.Json.Serialization;
-using Amazon.DynamoDBv2.DataModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace OrderFlow.Events;
 
-[DynamoDBTable("Events")]
 public class Event
 {
-    public string Id { get; } = Guid.NewGuid().ToString();
-    [JsonPropertyName("Event")] public string @event { get; set; }
-    public DateTime Timestamp { get; } = DateTime.Now;
+    [Key]    
+    public string Id { get; set; } = Guid.NewGuid().ToString();
+    public string @event { get; set; }
+    public DateTime Timestamp { get; } = DateTime.Now.ToUniversalTime();
     public string StreamId { get; set; }
     public string EventType { get; set; }
-    public long _deletionDate { get; } = ((DateTimeOffset)DateTime.Now.AddDays(7)).ToUnixTimeSeconds();
 }
