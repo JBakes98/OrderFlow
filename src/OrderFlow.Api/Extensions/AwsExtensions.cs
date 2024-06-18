6ada7f4845975dfc7ec8939ad9da31ee9417f3a1
JBakes98 <34064544+JBakes98@users.noreply.github.com>
2024-06-18 08:36:49 +0100

    Moved AWS registration into seperate extension class

---
+++ src/OrderFlow.Api/Extensions/AwsExtensions.cs
@@ -0,0 +1,14 @@
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
+
+namespace OrderFlow.Extensions;
+
+public static class AwsExtensions
+{
+    public static void RegisterAwsServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDefaultAWSOptions(configuration.GetAWSOptions());
+        services.AddAWSService<IAmazonDynamoDB>();
+        services.AddTransient<IDynamoDBContext, DynamoDBContext>();
+    }
+}
