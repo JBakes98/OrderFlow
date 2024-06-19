300dcba24634449b5ad986954706d0f0c537fea3
JBakes98 <34064544+JBakes98@users.noreply.github.com>
2024-06-19 11:47:44 +0100

    Remove dynamo tag from event object

--- src/OrderFlow.Api/Events/BaseOrderEvent.cs
+++ src/OrderFlow.Api/Events/BaseOrderEvent.cs
@@ -1,12 +1,7 @@
-using System.Text.Json.Serialization;
-using Amazon.DynamoDBv2.DataModel;
-
 namespace OrderFlow.Events;
 
-[JsonDerivedType(typeof(OrderCreatedEvent))]
 public class BaseOrderEvent
 {
-    [DynamoDBHashKey]
     public string EventId { get; } = Guid.NewGuid().ToString();
     public DateTime CreatedOn { get; set; }
     public string EventType { get; set; }
