1bad49860ff97898b2211b70018f7c22604b4eca
JBakes98 <34064544+JBakes98@users.noreply.github.com>
2024-06-20 07:51:58 +0100

    Remove authentication extension (will use cognito)

--- src/OrderFlow.Api/Extensions/AuthenticationExtensions.cs
+++ src/OrderFlow.Api/Extensions/AuthenticationExtensions.cs
@@ -11,33 +11,6 @@ public static class AuthenticationExtensions
 {
     public static void RegisterAuthentication(this WebApplicationBuilder builder, IConfiguration configuration)
     {
-        builder.Services.AddCors(options =>
-        {
-            options.AddPolicy("AllowSpecificOrigin",
-                policy =>
-                {
-                    policy.AllowAnyOrigin()
-                        .AllowAnyHeader()
-                        .AllowAnyMethod();
-                });
-        });
-        
-        var domain = $"https://{builder.Configuration["Auth0:Domain"]}/";
-        builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-            .AddJwtBearer(options =>
-            {
-                options.Authority = domain;
-                options.Audience = builder.Configuration["Auth0:Audience"];
-                options.TokenValidationParameters = new TokenValidationParameters
-                {
-                    ValidateIssuer = true,
-                    ValidIssuer = domain,
-                    ValidateAudience = true,
-                    ValidAudience = builder.Configuration["Auth0:Audience"],
-                    ValidateLifetime = true,
-                    NameClaimType = ClaimTypes.NameIdentifier
-                };
-            });
         
     }
 }
