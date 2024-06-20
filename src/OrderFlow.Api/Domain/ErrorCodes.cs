d65b91e72df24498f0e1a5948d7a4485080f1e34
JBakes98 <34064544+JBakes98@users.noreply.github.com>
2024-06-20 07:54:05 +0100

    Add domain error codes

--- src/OrderFlow.Api/Domain/ErrorCodes.cs
+++ src/OrderFlow.Api/Domain/ErrorCodes.cs
@@ -3,5 +3,8 @@ namespace OrderFlow.Domain;
 public static class ErrorCodes
 {
     public const string InstrumentNotFound = "instrument_not_found";
+    public const string InstrumentCouldNotBeCreated = "instrument_could_not_be_created";
+    public const string OrderCouldNotBeCreated = "order_could_not_be_created";
     public const string OrderNotFound = "order_not_found";
+    public const string EventCouldNotBePublished = "event_could_not_be_published";
 }
