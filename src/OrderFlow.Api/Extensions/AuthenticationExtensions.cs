89be04c9f775ee2e1bed8e41f1d896bba781ea90
JBakes98 <34064544+JBakes98@users.noreply.github.com>
2024-06-21 15:04:03 +0100

    register cognito authentication

--- src/OrderFlow.Api/Extensions/AuthenticationExtensions.cs
+++ src/OrderFlow.Api/Extensions/AuthenticationExtensions.cs
@@ -1,15 +1,30 @@
-using System.Security.Claims;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.IdentityModel.Tokens;
-using OrderFlow.Services;
-using OrderFlow.Services.Handlers;
 
 namespace OrderFlow.Extensions;
 
 public static class AuthenticationExtensions
 {
-    public static void RegisterAuthentication(this WebApplicationBuilder builder, IConfiguration configuration)
+    public static void RegisterAuthentication(this IServiceCollection services, IConfiguration configuration)
     {
+        services.AddCognitoIdentity();
+        services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.Authority = configuration["Cognito:Authority"];
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidIssuer = configuration["Cognito:Authority"],
+                    ValidateIssuerSigningKey = true,
+                    ValidateIssuer = true,
+                    ValidateLifetime = true,
+                    ValidAudience = "",
+                    ValidateAudience = false
+                };
+            });
     }
 }
