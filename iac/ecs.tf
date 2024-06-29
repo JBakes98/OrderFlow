653ce1c76c5c84dd375f03787f484c9ac81cfe7d
JBakes98 <34064544+JBakes98@users.noreply.github.com>
2024-06-29 20:58:41 +0100

    Use MiB value for CPU and Memory (#21)
    * Create ECR repo infra
    
    * Create build and push to ECR GHA
    
    * Create ECS infra
    
    * Create deploy to ECS action to deploy target tags
    
    * fix cpu issue
    
    * Use MiB value for CPU and Memory

--- iac/ecs.tf
+++ iac/ecs.tf
@@ -8,8 +8,8 @@ resource "aws_ecs_task_definition" "oderflow_api" {
   family                   = "orderflow-api-task"
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
-  cpu                      = ".5"
-  memory                   = "1"
+  cpu                      = "256"
+  memory                   = "512"
   execution_role_arn       = aws_iam_role.ecs_execution_role.arn
 
   container_definitions = jsonencode([{
