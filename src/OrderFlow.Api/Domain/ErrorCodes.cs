6f62819708d5fb03be3a29ab67583bd17ae80e99
JBakes98 <34064544+JBakes98@users.noreply.github.com>
2024-07-01 12:10:02 +0100

    Added error code for if the price for an isntrument cant be retrieved from Alpha Vantage

--- src/OrderFlow.Api/Domain/ErrorCodes.cs
+++ src/OrderFlow.Api/Domain/ErrorCodes.cs
@@ -6,5 +6,8 @@ public static class ErrorCodes
     public const string InstrumentCouldNotBeCreated = "instrument_could_not_be_created";
     public const string OrderCouldNotBeCreated = "order_could_not_be_created";
     public const string OrderNotFound = "order_not_found";
+
     public const string EventCouldNotBePublished = "event_could_not_be_published";
+
+    public const string UnableToRetrieveCurrentInstrumentPrice = "unable_to_retrieve_current_instrument_price";
 }
