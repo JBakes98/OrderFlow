4b1a97b789b1d9d3e3cd2deb40235240ebc89568
JBakes98 <34064544+JBakes98@users.noreply.github.com>
2024-07-01 08:25:18 +0100

    Create AlphaVantage configuration

---
+++ src/OrderFlow.Api/Options/AlphaVantageOptions.cs
@@ -0,0 +1,7 @@
+namespace OrderFlow.Options;
+
+public class AlphaVantageOptions
+{
+    public string ApiKey { get; set; } = String.Empty;
+    public string BasePath { get; set; } = String.Empty;
+}
