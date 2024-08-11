3ae4b93be23b5ec23d9401c99092cd4798de324c
JBakes98 <34064544+JBakes98@users.noreply.github.com>
2024-08-11 09:47:17 +0100

    Add S3 bucket for history files

---
+++ iac/s3.tf
@@ -0,0 +1,17 @@
+resource "aws_s3_bucket" "orderflow_history_bucket" {
+  bucket = "orderflow-bucket"
+
+  tags = {
+    Name        = "OrderFlow Bucket"
+    Environment = "Local"
+  }
+}
+
+resource "aws_s3_bucket_notification" "orderflow_history_bucket_notification" {
+  bucket = aws_s3_bucket.orderflow_history_bucket.id
+  
+  topic {
+    events    = ["s3:ObjectCreated:*"]
+    topic_arn = aws_sns_topic.orderflow_history_topic.arn
+  }
+}
