b7eaef7ba067fb12dffa79d718885e429d410914
JBakes98 <34064544+JBakes98@users.noreply.github.com>
2024-08-11 10:23:13 +0100

    Update controller to not use authorization for developing

--- src/OrderFlow.Api/Controllers/OrderController.cs
+++ src/OrderFlow.Api/Controllers/OrderController.cs
@@ -1,88 +1,82 @@
 using Ardalis.GuardClauses;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using OneOf;
 using OrderFlow.Contracts.Requests;
-using OrderFlow.Middleware;
 using OrderFlow.Models;
 using OrderFlow.Services;
 using OrderFlow.Services.Handlers;
 
-namespace OrderFlow.Controllers
+namespace OrderFlow.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class OrderController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class OrderController : ControllerBase
+    private readonly IHandler<CreateOrder, Order> _createHandler;
+    private readonly IHandler<string, Order> _getHandler;
+    private readonly IOrderService _orderService;
+
+    public OrderController(
+        IHandler<CreateOrder, Order> createHandler,
+        IHandler<string, Order> getHandler,
+        IOrderService orderService)
     {
-        private readonly IHandler<CreateOrder, Order> _createHandler;
-        private readonly IHandler<string, Order> _getHandler;
-        private readonly IOrderService _orderService;
+        _createHandler = Guard.Against.Null(createHandler);
+        _getHandler = Guard.Against.Null(getHandler);
+        _orderService = Guard.Against.Null(orderService);
+    }
 
-        public OrderController(
-            IHandler<CreateOrder, Order> createHandler,
-            IHandler<string, Order> getHandler,
-            IOrderService orderService)
-        {
-            _createHandler = Guard.Against.Null(createHandler);
-            _getHandler = Guard.Against.Null(getHandler);
-            _orderService = Guard.Against.Null(orderService);
-        }
+    private const string UserOrdersReadScope = "orderflow/read:data";
+    private const string UserOrdersWriteScope = "orderflow/write:data";
 
-        private const string UserOrdersReadScope = "orderflow/read:data";
-        private const string UserOrdersWriteScope = "orderflow/write:data";
+    // GET: api/Order
+    [HttpGet]
+    // [Authorize]
+    public async Task<IActionResult> GetOrder()
+    {
+        var results = await _orderService.RetrieveOrders();
 
-        // GET: api/Order
-        [HttpGet]
-        [Authorize]
-        [CustomScopeAuthorization(UserOrdersReadScope)]
-        public async Task<IActionResult> GetOrder()
-        {
-            var results = await _orderService.RetrieveOrders();
+        return QueryOrdersResponse(results);
+    }
 
-            return QueryOrdersResponse(results);
-        }
+    // GET: api/Order/5
+    [HttpGet("{id}")]
+    // [Authorize]
+    public async Task<IActionResult> GetOrder([FromRoute] Guid id, CancellationToken cancellationToken)
+    {
+        var result = await _getHandler.HandleAsync(id.ToString(), cancellationToken);
 
-        // GET: api/Order/5
-        [HttpGet("{id}")]
-        [Authorize]
-        [CustomScopeAuthorization(UserOrdersReadScope)]
-        public async Task<IActionResult> GetOrder([FromRoute] Guid id, CancellationToken cancellationToken)
-        {
-            var result = await _getHandler.HandleAsync(id.ToString(), cancellationToken);
+        return GetOrderResponse(result);
+    }
 
-            return GetOrderResponse(result);
-        }
+    // POST: api/Order
+    [HttpPost]
+    // [Authorize]
+    public async Task<IActionResult> PostOrder(CreateOrder request, CancellationToken cancellationToken)
+    {
+        var result = await _createHandler.HandleAsync(request, cancellationToken);
 
-        // POST: api/Order
-        [HttpPost]
-        [Authorize]
-        [CustomScopeAuthorization(UserOrdersWriteScope)]
-        public async Task<IActionResult> PostOrder(CreateOrder request, CancellationToken cancellationToken)
-        {
-            var result = await _createHandler.HandleAsync(request, cancellationToken);
+        return CreateOrderResponse(result);
+    }
 
-            return CreateOrderResponse(result);
-        }
+    private static IActionResult CreateOrderResponse(OneOf<Order, Error> result)
+    {
+        return result.Match<IActionResult>(
+            order => new ObjectResult(order),
+            error => new ObjectResult(error));
+    }
 
-        private static IActionResult CreateOrderResponse(OneOf<Order, Error> result)
-        {
-            return result.Match<IActionResult>(
-                order => new ObjectResult(order),
-                error => new ObjectResult(error));
-        }
+    private static IActionResult GetOrderResponse(OneOf<Order, Error> result)
+    {
+        return result.Match<IActionResult>(
+            order => new ObjectResult(order),
+            error => new ObjectResult(error));
+    }
 
-        private static IActionResult GetOrderResponse(OneOf<Order, Error> result)
-        {
-            return result.Match<IActionResult>(
-                order => new ObjectResult(order),
-                error => new ObjectResult(error));
-        }
-
-        private static IActionResult QueryOrdersResponse(OneOf<IEnumerable<Order>, Error> result)
-        {
-            return result.Match<IActionResult>(
-                orders => new ObjectResult(orders),
-                error => new ObjectResult(error));
-        }
+    private static IActionResult QueryOrdersResponse(OneOf<IEnumerable<Order>, Error> result)
+    {
+        return result.Match<IActionResult>(
+            orders => new ObjectResult(orders),
+            error => new ObjectResult(error));
     }
 }
