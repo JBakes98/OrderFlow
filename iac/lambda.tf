8fc59aeba6fc0bb65ddfe299d54c1da8224e00ce
JBakes98 <34064544+JBakes98@users.noreply.github.com>
2024-10-12 22:34:26 +0100

    Add lambda template IAC

---
+++ iac/lambda.tf
@@ -0,0 +1,37 @@
+/*module "orderflow_history_function" {
+  source = "terraform-aws-modules/lambda/aws"
+
+  function_name = "orderflow-history-function"
+  description   = "Lambda function to process order file from s3"
+  handler       = "OrderFlow.History.Processor::OrderFlow.History.Processor.Function::FunctionHandler"
+  runtime       = "dotnet8"
+
+  create_package         = false
+  local_existing_package = "../src/OrderFlow.History.Processor/bin/Release/net8.0/OrderFlow.History.Processor.zip"
+}
+
+resource "aws_lambda_permission" "orderflow_history_function_permission" {
+  statement_id  = "AllowS3Invoke"
+  action        = "lambda:InvokeFunction"
+  function_name = module.orderflow_history_function.lambda_function_name
+  principal     = "s3.amazonaws.com"
+  source_arn    = "arn:aws:s3:::${aws_s3_bucket.orderflow_history_bucket.id}"
+}
+
+module "orderflow_events_publisher_function" {
+  source = "terraform-aws-modules/lambda/aws"
+
+  function_name = "orderflow-events-publisher"
+  description   = "Lambda function that processes events Dynamo stream and publishes them to SNS"
+  handler       = "OrderFlow.Events.Publisher::OrderFlow.Events.Publisher.Function::FunctionHandler"
+  runtime       = "dotnet8"
+
+  create_package         = false
+  local_existing_package = "../src/OrderFlow.Events.Publisher/bin/Release/net8.0/OrderFlow.Events.Publisher.zip"
+}
+
+resource "aws_lambda_event_source_mapping" "orderflow_events_stream" {
+  function_name = module.orderflow_events_publisher_function.lambda_function_name
+  event_source_arn = aws_dynamodb_table.events_table.stream_arn
+  starting_position = "LATEST"
+}*/
