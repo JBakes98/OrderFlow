6f75d79cee1be4ef8109d3365574a50433e0502a
JBakes98 <34064544+JBakes98@users.noreply.github.com>
2024-10-12 22:32:44 +0100

    Add template for order history bucket notification

--- iac/s3.tf
+++ iac/s3.tf
@@ -7,11 +7,15 @@ resource "aws_s3_bucket" "orderflow_history_bucket" {
   }
 }
 
-resource "aws_s3_bucket_notification" "orderflow_history_bucket_notification" {
+/*resource "aws_s3_bucket_notification" "orderflow_history_bucket_notification" {
   bucket = aws_s3_bucket.orderflow_history_bucket.id
+  lambda_function {
+    lambda_function_arn = module.orderflow_history_function.lambda_function_arn
+    events              = ["s3:ObjectCreated:*"]
+  }
 
   topic {
     events    = ["s3:ObjectCreated:*"]
     topic_arn = aws_sns_topic.orderflow_history_topic.arn
   }
-}
+}*/
