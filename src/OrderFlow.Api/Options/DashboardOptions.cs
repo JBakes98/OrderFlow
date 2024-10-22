83f0c58fb5ad84fec59ba70d106da7e95afc2e04
JBakes98 <34064544+JBakes98@users.noreply.github.com>
2024-10-22 20:19:46 +0100

    Add dashboard config class

---
+++ src/OrderFlow.Api/Options/DashboardOptions.cs
@@ -0,0 +1,10 @@
+namespace OrderFlow.Options;
+
+public class DashboardOptions
+{
+    public string Origin { get; set; } = string.Empty;
+    public string Name { get; set; } = string.Empty;
+    public bool AllowAnyHeaders { get; set; } = false;
+    public bool AllowAnyMethod { get; set; } = false;
+    public bool AllowCredentials { get; set; } = false;
+}
