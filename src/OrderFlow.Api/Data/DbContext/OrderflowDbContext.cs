8786bdfd849bb1dd4d3ddb3ce61cc813c7dbd5e4
JBakes98 <34064544+JBakes98@users.noreply.github.com>
2024-10-23 20:49:18 +0100

    Update db context for new objects

--- src/OrderFlow.Api/Data/DbContext/OrderflowDbContext.cs
+++ src/OrderFlow.Api/Data/DbContext/OrderflowDbContext.cs
@@ -1,11 +1,11 @@
 using Microsoft.EntityFrameworkCore;
-using OrderFlow.Domain.Models;
+using OrderFlow.Data.Entities;
 
 namespace OrderFlow.Data.DbContext;
 
 public class OrderflowDbContext(DbContextOptions<OrderflowDbContext> options)
     : Microsoft.EntityFrameworkCore.DbContext(options)
 {
-    public DbSet<Instrument> Instruments { get; set; }
-    public DbSet<Order> Orders { get; set; }
+    public DbSet<Instrument> Instruments { get; init; }
+    public DbSet<Order> Orders { get; init; }
 }
