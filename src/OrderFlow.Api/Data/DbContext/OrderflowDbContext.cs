0f8cf71a63f0c3bec7221ad4c01f1318ae041a98
JBakes98 <34064544+JBakes98@users.noreply.github.com>
2024-10-24 08:29:35 +0100

    Update db context to use the new entity objects

--- src/OrderFlow.Api/Data/DbContext/OrderflowDbContext.cs
+++ src/OrderFlow.Api/Data/DbContext/OrderflowDbContext.cs
@@ -6,6 +6,6 @@ namespace OrderFlow.Data.DbContext;
 public class OrderflowDbContext(DbContextOptions<OrderflowDbContext> options)
     : Microsoft.EntityFrameworkCore.DbContext(options)
 {
-    public DbSet<Instrument> Instruments { get; init; }
-    public DbSet<Order> Orders { get; init; }
+    public DbSet<InstrumentEntity> Instruments { get; set; }
+    public DbSet<OrderEntity> Orders { get; set; }
 }
