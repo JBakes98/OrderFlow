f6ef430e6095cab73ea9c5c00d1c6787338cf537
JBakes98 <34064544+JBakes98@users.noreply.github.com>
2024-10-24 08:28:18 +0100

    Update entities to include empty constructor needed for ef

--- src/OrderFlow.Api/Data/Entities/InstrumentEntity.cs
+++ src/OrderFlow.Api/Data/Entities/InstrumentEntity.cs
@@ -1,15 +1,30 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace OrderFlow.Data.Entities;
 
-public class InstrumentEntity(
-    string id,
-    string ticker,
-    string name,
-    string sector,
-    string exchange)
+public class InstrumentEntity
 {
-    public string Id { get; } = id;
-    public string Ticker { get; } = ticker;
-    public string Name { get; } = name;
-    public string Sector { get; } = sector;
-    public string Exchange { get; } = exchange;
+    public InstrumentEntity()
+    {
+    }
+
+    public InstrumentEntity(
+        string id,
+        string ticker,
+        string name,
+        string sector,
+        string exchange)
+    {
+        Id = id;
+        Ticker = ticker;
+        Name = name;
+        Sector = sector;
+        Exchange = exchange;
+    }
+
+    [Key] public string Id { get; private set; }
+    public string Ticker { get; private set; }
+    public string Name { get; private set; }
+    public string Sector { get; private set; }
+    public string Exchange { get; private set; }
 }
--- src/OrderFlow.Api/Data/Entities/OrderEntity.cs
+++ src/OrderFlow.Api/Data/Entities/OrderEntity.cs
@@ -1,15 +1,30 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace OrderFlow.Data.Entities;
 
-public class OrderEntity(
-    string id,
-    int quantity,
-    string instrumentId,
-    double price,
-    DateTime orderDate)
+public class OrderEntity
 {
-    public string Id { get; } = id;
-    public int Quantity { get; } = quantity;
-    public string InstrumentId { get; } = instrumentId;
-    public double Price { get; } = price;
-    public DateTime OrderDate { get; } = orderDate;
+    public OrderEntity()
+    {
+    }
+
+    public OrderEntity(
+        string id,
+        int quantity,
+        string instrumentId,
+        double price,
+        DateTime orderDate)
+    {
+        Id = id;
+        Quantity = quantity;
+        InstrumentId = instrumentId;
+        Price = price;
+        OrderDate = orderDate;
+    }
+
+    [Key] public string Id { get; private set; }
+    public int Quantity { get; private set; }
+    public string InstrumentId { get; private set; }
+    public double Price { get; private set; }
+    public DateTime OrderDate { get; private set; }
 }
