da20bcb915694558eada82afe77a656b32963278
JBakes98 <34064544+JBakes98@users.noreply.github.com>
2024-11-01 20:55:17 +0000

    Add new endpoint to get instrument orders

--- src/OrderFlow.Api/Controllers/InstrumentController.cs
+++ src/OrderFlow.Api/Controllers/InstrumentController.cs
@@ -1,3 +1,4 @@
+using Ardalis.GuardClauses;
 using Microsoft.AspNetCore.Mvc;
 using OneOf;
 using OrderFlow.Contracts.Requests;
@@ -14,16 +15,18 @@ public class InstrumentController : ControllerBase
     private readonly IHandler<CreateInstrument, Instrument> _createHandler;
     private readonly IHandler<string, Instrument> _getInstrumentHandler;
     private readonly IInstrumentService _instrumentService;
+    private readonly IOrderService _orderService;
 
     public InstrumentController(
         IHandler<CreateInstrument, Instrument> createHandler,
         IHandler<string, Instrument> getInstrumentHandler,
-        IInstrumentService instrumentService
-    )
+        IInstrumentService instrumentService,
+        IOrderService orderService)
     {
-        _createHandler = createHandler;
-        _getInstrumentHandler = getInstrumentHandler;
-        _instrumentService = instrumentService;
+        _orderService = Guard.Against.Null(orderService);
+        _createHandler = Guard.Against.Null(createHandler);
+        _getInstrumentHandler = Guard.Against.Null(getInstrumentHandler);
+        _instrumentService = Guard.Against.Null(instrumentService);
     }
 
     private const string UserInstrumentsReadScope = "orderflow/read:data";
@@ -47,6 +50,16 @@ public class InstrumentController : ControllerBase
         var result = await _getInstrumentHandler.HandleAsync(id.ToString(), cancellationToken);
 
         return GetInstrumentResponse(result);
+    }
+
+    // GET api/Instruments/5/Orders
+    [HttpGet("{id}/Orders")]
+    // [Authorize]
+    public async Task<IActionResult> GetInstrumentOrders([FromRoute] Guid id, CancellationToken cancellationToken)
+    {
+        var result = await _orderService.RetrieveInstrumentOrders(id.ToString());
+
+        return QueryOrdersResponse(result);
     }
 
     // POST api/<InstrumentController>
@@ -77,6 +90,13 @@ public class InstrumentController : ControllerBase
     {
         return result.Match<IActionResult>(
             instruments => new ObjectResult(instruments),
+            error => new ObjectResult(error));
+    }
+
+    private static IActionResult QueryOrdersResponse(OneOf<IEnumerable<Order>, Error> result)
+    {
+        return result.Match<IActionResult>(
+            orders => new ObjectResult(orders),
             error => new ObjectResult(error));
     }
 }
