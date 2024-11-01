ca9c549b02e6aaf4b939940d1dc76a4b0766b557
JBakes98 <34064544+JBakes98@users.noreply.github.com>
2024-11-01 21:12:33 +0000

    Remove dynamodb tables that are moved to postgres

--- iac/dynamo.tf
+++ iac/dynamo.tf
@@ -1,29 +1,3 @@
-resource "aws_dynamodb_table" "order_table" {
-  name           = "Order"
-  billing_mode   = "PROVISIONED"
-  hash_key       = "Id"
-  read_capacity  = 5 # Adjust according to your read/write throughput requirements
-  write_capacity = 5
-
-  attribute {
-    name = "Id"
-    type = "S"
-  }
-}
-
-resource "aws_dynamodb_table" "instrument_table" {
-  name           = "Instrument"
-  billing_mode   = "PROVISIONED"
-  hash_key       = "Id"
-  read_capacity  = 5 # Adjust according to your read/write throughput requirements
-  write_capacity = 5
-
-  attribute {
-    name = "Id"
-    type = "S"
-  }
-}
-
 resource "aws_dynamodb_table" "events_table" {
   name           = "OrderEvents"
   billing_mode   = "PROVISIONED"
