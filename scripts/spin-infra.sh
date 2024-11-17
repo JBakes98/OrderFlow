31dcd397b5db6c34ebe1d57096b4a5a8129cbcf7
JBakes98 <34064544+JBakes98@users.noreply.github.com>
2024-11-17 16:23:25 +0000

    Update infra script to update rds instance to apply logical wal level

--- scripts/spin-infra.sh
+++ scripts/spin-infra.sh
@@ -13,3 +13,12 @@ cd ./iac
 terraform init
 tflocal apply -auto-approve
 
+# Force restart the RDS postgres instance to apply the parameter group
+awslocal rds modify-db-instance --db-instance-identifier orderflow-postgres-instance --db-parameter-group-name "orderflow-rds-pg" > /dev/null 2>&1
+
+# Move to the API directory
+cd ../src/Orderflow.Api/
+
+# Run entity framework update command to create SQL tables
+dotnet ef database update 
+
