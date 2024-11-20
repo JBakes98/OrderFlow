14e3be3a6916e90496a8dab7cd1db33e5f5852df
JBakes98 <34064544+JBakes98@users.noreply.github.com>
2024-11-20 22:03:56 +0000

    Update to using eu-west-1 aws az

--- iac/vpc.tf
+++ iac/vpc.tf
@@ -12,7 +12,7 @@ resource "aws_vpc" "orderflow_api_vpc" {
 resource "aws_subnet" "public_subnet_az1" {
   vpc_id     = aws_vpc.orderflow_api_vpc.id
   cidr_block = "10.0.1.0/24"
-  availability_zone = "eu-west-2a"
+  availability_zone = "eu-west-1a"
 
   tags = {
     Name = "Public Subnet 1"
@@ -23,7 +23,7 @@ resource "aws_subnet" "public_subnet_az1" {
 resource "aws_subnet" "public_subnet_az2" {
   vpc_id     = aws_vpc.orderflow_api_vpc.id
   cidr_block = "10.0.2.0/24"
-  availability_zone = "eu-west-2b"
+  availability_zone = "eu-west-1b"
 
   tags = {
     Name = "Public Subnet 2"
@@ -34,7 +34,7 @@ resource "aws_subnet" "public_subnet_az2" {
 resource "aws_subnet" "private_subnet_az1" {
   vpc_id     = aws_vpc.orderflow_api_vpc.id
   cidr_block = "10.0.3.0/24"
-  availability_zone = "eu-west-2a"
+  availability_zone = "eu-west-1a"
 
   tags = {
     Name = "Private Subnet 1"
@@ -45,7 +45,7 @@ resource "aws_subnet" "private_subnet_az1" {
 resource "aws_subnet" "private_subnet_az2" {
   vpc_id     = aws_vpc.orderflow_api_vpc.id
   cidr_block = "10.0.4.0/24"
-  availability_zone = "eu-west-2b"
+  availability_zone = "eu-west-1b"
 
   tags = {
     Name = "Private Subnet 2"
