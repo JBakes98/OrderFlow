5827fc947d2c24cdd77bf55fc4e5ced802302ad5
JBakes98 <34064544+JBakes98@users.noreply.github.com>
2024-11-20 22:01:37 +0000

    delete ecr repos that will be in lambda repos

--- iac/ecr.tf
+++ iac/ecr.tf
@@ -2,14 +2,3 @@ resource "aws_ecr_repository" "orderflow_api_repo" {
   name                 = "orderflow-api" 
   image_tag_mutability = "MUTABLE"
 }
-
-resource "aws_ecr_repository" "orderflow_events_publisher_repo" {
-  name                 = "orderflow-events-publisher"
-  image_tag_mutability = "MUTABLE"
-}
-
-
-resource "aws_ecr_repository" "orderflow_history_processor_repo" {
-  name                 = "orderflow-history-processor"
-  image_tag_mutability = "MUTABLE"
-}
