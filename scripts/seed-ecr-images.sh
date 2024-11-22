e8a6b5824202f4538fb333307247b18a1dfa119f
JBakes98 <34064544+JBakes98@users.noreply.github.com>
2024-11-22 17:16:48 +0000

    change script to correct aws region

--- scripts/seed-ecr-images.sh
+++ scripts/seed-ecr-images.sh
@@ -1,10 +1,10 @@
 #!/bin/zsh
 
 # Build Orderflow Events Publisher Lambda Docker image
-docker build -t "000000000000.dkr.ecr.eu-west-2.localhost.localstack.cloud:4566/orderflow-events-publisher" . -f src/OrderFlow.Events.Publisher/Dockerfile
-docker push "000000000000.dkr.ecr.eu-west-2.localhost.localstack.cloud:4566/orderflow-events-publisher"
+docker build -t "000000000000.dkr.ecr.eu-west-1.localhost.localstack.cloud:4566/orderflow-events-publisher" . -f src/OrderFlow.Events.Publisher/Dockerfile
+docker push "000000000000.dkr.ecr.eu-west-1.localhost.localstack.cloud:4566/orderflow-events-publisher"
 
 
 # Build Orderflow API Docker image 
-docker build -t "000000000000.dkr.ecr.eu-west-2.localhost.localstack.cloud:4566/orderflow-api" . -f src/OrderFlow.Api/Dockerfile
-docker push "000000000000.dkr.ecr.eu-west-2.localhost.localstack.cloud:4566/orderflow-api"
+docker build -t "000000000000.dkr.ecr.eu-west-1.localhost.localstack.cloud:4566/orderflow-api" . -f src/OrderFlow.Api/Dockerfile
+docker push "000000000000.dkr.ecr.eu-west-1.localhost.localstack.cloud:4566/orderflow-api"
