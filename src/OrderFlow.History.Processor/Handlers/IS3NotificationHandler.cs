7133aaa2ba2b097ea127cdb433c3ee0d82fd64ae
JBakes98 <34064544+JBakes98@users.noreply.github.com>
2024-11-22 16:54:46 +0000

    Remove unused lambda functions

--- src/OrderFlow.History.Processor/Handlers/IS3NotificationHandler.cs
+++
@@ -1,8 +0,0 @@
-using Amazon.Lambda.S3Events;
-
-namespace OrderFlowHistoryFunction.Handlers;
-
-public interface IS3NotificationHandler
-{
-    Task<bool> HandleAsync(S3Event.S3EventNotificationRecord notification);
-}
