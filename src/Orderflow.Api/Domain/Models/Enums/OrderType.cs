888fbff01036aa0b217fa71db48146c74fb0424a
JBakes98 <34064544+JBakes98@users.noreply.github.com>
2024-11-28 08:36:16 +0000

    Remove converter from enum type

--- src/Orderflow.Api/Domain/Models/Enums/OrderType.cs
+++ src/Orderflow.Api/Domain/Models/Enums/OrderType.cs
@@ -1,8 +1,5 @@
-using System.Text.Json.Serialization;
-
 namespace Orderflow.Domain.Models.Enums;
 
-[JsonConverter(typeof(JsonStringEnumConverter<OrderStatus>))]
 public enum OrderType
 {
     buy,
