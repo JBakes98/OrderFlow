74822b0afa2a91851a9b1b018a7c7d72cd9a3106
JBakes98 <34064544+JBakes98@users.noreply.github.com>
2024-12-11 16:18:55 +0000

    Register mappers

--- src/Orderflow.Api/Extensions/ServiceExtensions.cs
+++ src/Orderflow.Api/Extensions/ServiceExtensions.cs
@@ -3,8 +3,10 @@ using Orderflow.Api.Routes.Order.Models;
 using Orderflow.Data.Entities;
 using Orderflow.Data.Repositories;
 using Orderflow.Data.Repositories.Interfaces;
+using Orderflow.Domain.Commands;
 using Orderflow.Domain.Models;
 using Orderflow.Events;
+using Orderflow.Mappers;
 using Orderflow.Mappers.Data;
 using Orderflow.Mappers.Domain;
 using Orderflow.Mappers.Events;
@@ -29,10 +31,12 @@ public static class ServiceExtensions
         services.AddScoped<IOrderService, OrderService>();
 
         services.AddScoped<IMapper<PostOrderRequest, Order>, PostOrderRequestToOrderMapper>();
+        services.AddScoped<IMapper<PutOrderRequest, OrderUpdateCommand>, PutOrderRequestToOrderUpdateCommandMapper>();
         services.AddScoped<IMapper<Order, GetOrderResponse>, OrderToGetOrderResponseMapper>();
         services.AddScoped<IMapper<Order, OrderEntity>, OrderDomainToOrderDataMapper>();
         services.AddScoped<IMapper<OrderEntity, Order>, OrderDataToOrderDomainMapper>();
         services.AddScoped<IMapper<Order, OrderRaisedEvent>, OrderToOrderRaisedEventMapper>();
+        services.AddScoped<IMapper<OrderUpdateCommand, OrderUpdateEvent>, OrderUpdateToOrderUpdateEventMapper>();
 
         services.AddScoped<IMapper<PostInstrumentRequest, Instrument>, PostInstrumentRequestToInstrumentMapper>();
         services.AddScoped<IMapper<Instrument, GetInstrumentResponse>, InstrumentToGetInstrumentResponseMapper>();
