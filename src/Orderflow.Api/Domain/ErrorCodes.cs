5c1cb3efb0583dbab9b401208785f79cd053ff01
JBakes98 <34064544+JBakes98@users.noreply.github.com>
2024-12-11 16:06:25 +0000

    Create new error code for OrderCouldNotBeUpdated

--- src/Orderflow.Api/Domain/ErrorCodes.cs
+++ src/Orderflow.Api/Domain/ErrorCodes.cs
@@ -5,9 +5,8 @@ public static class ErrorCodes
     public const string InstrumentNotFound = "instrument_not_found";
     public const string InstrumentCouldNotBeCreated = "instrument_could_not_be_created";
     public const string OrderCouldNotBeCreated = "order_could_not_be_created";
+    public const string OrderCouldNotBeUpdated = "order_could_not_be_updated";
     public const string OrderNotFound = "order_not_found";
-
-    public const string EventCouldNotBePublished = "event_could_not_be_published";
 
     public const string OrderFileUploadFailed = "order_file_upload_failed";
     public const string SpecifiedBucketDoesNotExist = "specified_bucket_does_not_exist";
