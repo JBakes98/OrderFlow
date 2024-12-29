098838de1819da9978abe411d5d0b7194a7968bd
JBakes98 <34064544+JBakes98@users.noreply.github.com>
2024-12-29 19:04:07 +0000

    Update Put handler to validate request with new validator

--- src/Orderflow.Api/Api/Routes/Order/Endpoints/PutOrder.cs
+++ src/Orderflow.Api/Api/Routes/Order/Endpoints/PutOrder.cs
@@ -1,3 +1,5 @@
+using System.Net;
+using FluentValidation;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using Orderflow.Api.Routes.Order.Models;
@@ -11,12 +13,19 @@ public static class PutOrder
 {
     public static async Task<Results<Ok<GetOrderResponse>, ProblemHttpResult>> Handle(
         IOrderService orderService,
+        IValidator<PutOrderRequest> validator,
         IMapper<PutOrderRequest, OrderUpdateCommand> putOrderRequestToOrderUpdateCommandMapper,
         IMapper<Domain.Models.Order, GetOrderResponse> orderToOrderResponseMapper,
         [FromBody] PutOrderRequest request,
         string id)
     {
         request.SetOrderId(id);
+
+        var validationResult = await validator.ValidateAsync(request);
+        if (!validationResult.IsValid)
+            return TypedResults.Problem(string.Join(",", validationResult.Errors),
+                statusCode: (int)HttpStatusCode.BadRequest);
+
         var command = putOrderRequestToOrderUpdateCommandMapper.Map(request);
 
         var result = await orderService.UpdateOrder(command);
