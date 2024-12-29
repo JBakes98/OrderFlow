79c179cc2c17ec23443059122f4371ffe4d88214
JBakes98 <34064544+JBakes98@users.noreply.github.com>
2024-12-29 19:03:22 +0000

    Change enum to string

--- src/Orderflow.Api/Api/Routes/Order/Models/PutOrderRequest.cs
+++ src/Orderflow.Api/Api/Routes/Order/Models/PutOrderRequest.cs
@@ -1,19 +1,17 @@
-using Orderflow.Domain.Models.Enums;
-
 namespace Orderflow.Api.Routes.Order.Models;
 
 public class PutOrderRequest
 {
     public PutOrderRequest(
         string id,
-        OrderStatus status)
+        string status)
     {
         Id = id;
         Status = status;
     }
 
     public string Id { get; private set; }
-    public OrderStatus Status { get; }
+    public string Status { get; }
 
     public void SetOrderId(string id)
     {
