91fc8f78a42c67ce6385abb85bd1c4cfee127799
JBakes98 <34064544+JBakes98@users.noreply.github.com>
2025-01-09 19:57:30 +0000

    Update directory structure

--- src/Orderflow.Api/Services/IOrderBook.cs
+++
@@ -1,9 +0,0 @@
-using Orderflow.Domain.Models;
-
-namespace Orderflow.Services;
-
-public interface IOrderBook
-{
-    public List<Trade> AddOrder(Order order);
-    public void CancelOrders(List<string> orderIds);
-}
