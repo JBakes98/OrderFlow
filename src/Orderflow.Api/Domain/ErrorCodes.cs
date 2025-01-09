ddf3a67ecf394bc58aaaf0bc6fb57b150b87b7e9
JBakes98 <34064544+JBakes98@users.noreply.github.com>
2025-01-09 20:07:19 +0000

    Add trade error

--- src/Orderflow.Api/Domain/ErrorCodes.cs
+++ src/Orderflow.Api/Domain/ErrorCodes.cs
@@ -8,6 +8,7 @@ public static class ErrorCodes
 
     public const string InstrumentNotFound = "instrument_not_found";
     public const string InstrumentCouldNotBeCreated = "instrument_could_not_be_created";
+
     public const string OrderCouldNotBeCreated = "order_could_not_be_created";
     public const string OrderCouldNotBeUpdated = "order_could_not_be_updated";
     public const string OrderNotFound = "order_not_found";
@@ -16,4 +17,6 @@ public static class ErrorCodes
     public const string SpecifiedBucketDoesNotExist = "specified_bucket_does_not_exist";
 
     public const string UnableToRetrieveCurrentInstrumentPrice = "unable_to_retrieve_current_instrument_price";
+
+    public const string TradeExecutionFailed = "trade_execution_failed";
 }
