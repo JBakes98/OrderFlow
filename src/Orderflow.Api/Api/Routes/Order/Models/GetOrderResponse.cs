76b1b7b4426cc08128129f9dd1b7c2fa3b496ced
JBakes98 <34064544+JBakes98@users.noreply.github.com>
2025-01-18 20:49:23 +0000

    Add RemainingQuantity to GetOrderResponse

--- src/Orderflow.Api/Api/Routes/Order/Models/GetOrderResponse.cs
+++ src/Orderflow.Api/Api/Routes/Order/Models/GetOrderResponse.cs
@@ -7,32 +7,35 @@ public class GetOrderResponse
     public GetOrderResponse(
         string id,
         int quantity,
+        int remainingQuantity,
         string instrumentId,
         double price,
         double value,
         DateTime placed,
         DateTime updated,
-        TradeSide type,
+        TradeSide side,
         OrderStatus status)
     {
         Id = id;
         Quantity = quantity;
+        RemainingQuantity = remainingQuantity;
         InstrumentId = instrumentId;
         Price = price;
         Value = value;
         Placed = placed;
         Updated = updated;
-        Type = type;
+        Side = side;
         Status = status;
     }
 
     public string Id { get; }
     public int Quantity { get; }
+    public int RemainingQuantity { get; }
     public string InstrumentId { get; }
     public double Price { get; }
     public DateTime Placed { get; }
     public DateTime Updated { get; }
-    public TradeSide Type { get; }
+    public TradeSide Side { get; }
     public OrderStatus Status { get; }
     public double Value { get; }
 }
