59e7cdd93c0e116700b9da1c324b6ee29952d0bc
JBakes98 <34064544+JBakes98@users.noreply.github.com>
2025-01-21 22:26:27 +0000

    Add Authorization to endpoints to with cognito (#45)
    * Add Authorization to exchange endpoints to test cognito
    
    * Add authorization

--- src/Orderflow.Api/Api/Routes/Instrument/InstrumentUserGroup.cs
+++ src/Orderflow.Api/Api/Routes/Instrument/InstrumentUserGroup.cs
@@ -1,3 +1,4 @@
+using Orderflow.Api.Authorization;
 using Orderflow.Api.Routes.Instrument.Endpoints;
 
 namespace Orderflow.Api.Routes.Instrument;
@@ -6,16 +7,21 @@ public static class InstrumentUserGroup
 {
     public static void MapInstrumentUserGroup(this WebApplication app)
     {
+        var extraRequiredPolicies = Array.Empty<string>();
+
         var group = app.MapUserGroup("instruments");
 
         group.MapGet("/{id}", GetInstrument.Handle)
+            .RequireAuthorization([AuthorizationPolicy.Admin, .. extraRequiredPolicies])
             .WithSummary("Get a instrument");
 
         group.MapGet("/", ListInstrument.Handle)
+            .RequireAuthorization([AuthorizationPolicy.Admin, .. extraRequiredPolicies])
             .WithSummary("List instruments");
 
 
         group.MapPost("/", PostInstrument.Handle)
+            .RequireAuthorization([AuthorizationPolicy.Admin, .. extraRequiredPolicies])
             .WithSummary("Create a instrument");
     }
 }
