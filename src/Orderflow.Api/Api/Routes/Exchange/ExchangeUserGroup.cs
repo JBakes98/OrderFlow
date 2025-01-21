6581f21b1a937cc27e160582f591daaaab4b7d13
JBakes98 <34064544+JBakes98@users.noreply.github.com>
2025-01-21 08:26:32 +0000

    Add Authorization to exchange endpoints to test cognito

--- src/Orderflow.Api/Api/Routes/Exchange/ExchangeUserGroup.cs
+++ src/Orderflow.Api/Api/Routes/Exchange/ExchangeUserGroup.cs
@@ -1,3 +1,4 @@
+using Orderflow.Api.Authorization;
 using Orderflow.Api.Routes.Exchange.Endpoints;
 
 namespace Orderflow.Api.Routes.Exchange;
@@ -6,14 +7,19 @@ public static class ExchangeUserGroup
 {
     public static void MapExchangeUserGroup(this WebApplication app)
     {
+        var extraRequiredPolicies = new string[0];
+
         var group = app.MapUserGroup("exchanges");
 
         group.MapGet("/{id}", GetExchange.Handle)
+            .RequireAuthorization([AuthorizationPolicy.Admin, .. extraRequiredPolicies])
             .WithSummary("Get a exchange");
         group.MapGet("/", ListExchange.Handle)
+            .RequireAuthorization([AuthorizationPolicy.Admin, .. extraRequiredPolicies])
             .WithSummary("List exchanges");
 
         group.MapPost("/", PostExchange.Handle)
+            .RequireAuthorization(AuthorizationPolicy.Admin)
             .WithSummary("Create a exchange");
     }
 }
